c53d3b35177a4b9895aa134db00d029b0210625b
ModerateWinGuy <moderatewinguy@gmail.com>
2015-07-13 21:08:44 +1200

    Stuff

---
+++ VoxVisio/VoxVisio/Command.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace VoxVisio
+{
+    class Command
+    {
+        private string VoiceKeyword;
+        private KeyCombo keyCombo;
+    }
+
+    class KeyCombo
+    {
+        public List<VirtualKeyCode> Keys;
+        private readonly InputSimulator inputSimulator;
+
+        public KeyCombo(string keysString, InputSimulator inputSimulator)
+        {
+            this.inputSimulator = inputSimulator;
+            foreach (string VARIABLE in keysString.Split(','))
+            {
+                var keyCode = KeyTranslater.GetKeyCode(VARIABLE);
+                Keys.Add(keyCode);
+            }
+        }
+
+        public void PressKeys()
+        {
+            PressDownKeys();
+            ReleaseHeldKeys();
+        }
+
+        public void PressDownKeys()
+        {
+            foreach (VirtualKeyCode virtualKeyCode in Keys)
+            {
+                inputSimulator.Keyboard.KeyDown(virtualKeyCode);
+            }
+        }
+
+        public void ReleaseHeldKeys()
+        {
+            foreach (VirtualKeyCode virtualKeyCode in Keys)
+            {
+                inputSimulator.Keyboard.KeyUp(virtualKeyCode);
+            }
+        }
+    }
+}
--- VoxVisio/VoxVisio/KeyTranslater.cs
+++ VoxVisio/VoxVisio/KeyTranslater.cs
@@ -6,12 +6,15 @@ using WindowsInput.Native;
 
 namespace VoxVisio
 {
-    struct KeyCombo
+    /// <summary>
+    /// Represents a Virtual keycode and the string used to refer to it.
+    /// </summary>
+    struct KeyRep
     {
         public string Key;
         public VirtualKeyCode KeyCode;
         //public 
-        public KeyCombo(string key, VirtualKeyCode keyCode)
+        public KeyRep(string key, VirtualKeyCode keyCode)
         {
             Key = key;
             KeyCode = keyCode;
@@ -19,76 +22,76 @@ namespace VoxVisio
     }
     class KeyTranslater
     {
-        private KeyCombo[] keyCombos =
+        private static KeyRep[] _keyReps =
         {
-            new KeyCombo("m1", VirtualKeyCode.LBUTTON),
-            new KeyCombo("m2", VirtualKeyCode.RBUTTON),
-            new KeyCombo("m3", VirtualKeyCode.MBUTTON),
-            new KeyCombo("f1", VirtualKeyCode.F1),
-            new KeyCombo("f2", VirtualKeyCode.F2),
-            new KeyCombo("f3", VirtualKeyCode.F3),
-            new KeyCombo("f4", VirtualKeyCode.F4),
-            new KeyCombo("f5", VirtualKeyCode.F5),
-            new KeyCombo("f6", VirtualKeyCode.F6),
-            new KeyCombo("f7", VirtualKeyCode.F7),
-            new KeyCombo("f8", VirtualKeyCode.F8),
-            new KeyCombo("f9", VirtualKeyCode.F9),
-            new KeyCombo("f10", VirtualKeyCode.F10),
-            new KeyCombo("f11", VirtualKeyCode.F11),
-            new KeyCombo("f12", VirtualKeyCode.F12),
-            new KeyCombo("tab", VirtualKeyCode.TAB),
-            new KeyCombo("ctrl", VirtualKeyCode.CONTROL),
-            new KeyCombo("alt", VirtualKeyCode.MENU),
-            new KeyCombo("a", VirtualKeyCode.VK_A), 
-            new KeyCombo("b", VirtualKeyCode.VK_B), 
-            new KeyCombo("c", VirtualKeyCode.VK_C), 
-            new KeyCombo("d", VirtualKeyCode.VK_D), 
-            new KeyCombo("e", VirtualKeyCode.VK_E), 
-            new KeyCombo("f", VirtualKeyCode.VK_F), 
-            new KeyCombo("g", VirtualKeyCode.VK_G), 
-            new KeyCombo("h", VirtualKeyCode.VK_H), 
-            new KeyCombo("i", VirtualKeyCode.VK_I), 
-            new KeyCombo("j", VirtualKeyCode.VK_J), 
-            new KeyCombo("k", VirtualKeyCode.VK_K), 
-            new KeyCombo("l", VirtualKeyCode.VK_L), 
-            new KeyCombo("m", VirtualKeyCode.VK_M), 
-            new KeyCombo("n", VirtualKeyCode.VK_N), 
-            new KeyCombo("o", VirtualKeyCode.VK_O), 
-            new KeyCombo("p", VirtualKeyCode.VK_P), 
-            new KeyCombo("q", VirtualKeyCode.VK_Q), 
-            new KeyCombo("r", VirtualKeyCode.VK_R), 
-            new KeyCombo("s", VirtualKeyCode.VK_S), 
-            new KeyCombo("t", VirtualKeyCode.VK_T), 
-            new KeyCombo("u", VirtualKeyCode.VK_U), 
-            new KeyCombo("v", VirtualKeyCode.VK_V), 
-            new KeyCombo("w", VirtualKeyCode.VK_W), 
-            new KeyCombo("x", VirtualKeyCode.VK_X), 
-            new KeyCombo("y", VirtualKeyCode.VK_Y), 
-            new KeyCombo("z", VirtualKeyCode.VK_X), 
-            new KeyCombo("1", VirtualKeyCode.VK_1), 
-            new KeyCombo("2", VirtualKeyCode.VK_2), 
-            new KeyCombo("3", VirtualKeyCode.VK_3), 
-            new KeyCombo("4", VirtualKeyCode.VK_4), 
-            new KeyCombo("5", VirtualKeyCode.VK_5), 
-            new KeyCombo("6", VirtualKeyCode.VK_6), 
-            new KeyCombo("7", VirtualKeyCode.VK_7), 
-            new KeyCombo("8", VirtualKeyCode.VK_8), 
-            new KeyCombo("9", VirtualKeyCode.VK_9), 
-            new KeyCombo("0", VirtualKeyCode.VK_0), 
-            new KeyCombo("-", VirtualKeyCode.OEM_MINUS), 
-            new KeyCombo("+", VirtualKeyCode.OEM_PLUS), 
-            new KeyCombo("win", VirtualKeyCode.LWIN),
-            new KeyCombo("enter", VirtualKeyCode.RETURN),
-            new KeyCombo("left", VirtualKeyCode.LEFT),
-            new KeyCombo("right", VirtualKeyCode.RIGHT),
-            new KeyCombo("up", VirtualKeyCode.UP),
-            new KeyCombo("down", VirtualKeyCode.DOWN)
+            new KeyRep("m1", VirtualKeyCode.LBUTTON),
+            new KeyRep("m2", VirtualKeyCode.RBUTTON),
+            new KeyRep("m3", VirtualKeyCode.MBUTTON),
+            new KeyRep("f1", VirtualKeyCode.F1),
+            new KeyRep("f2", VirtualKeyCode.F2),
+            new KeyRep("f3", VirtualKeyCode.F3),
+            new KeyRep("f4", VirtualKeyCode.F4),
+            new KeyRep("f5", VirtualKeyCode.F5),
+            new KeyRep("f6", VirtualKeyCode.F6),
+            new KeyRep("f7", VirtualKeyCode.F7),
+            new KeyRep("f8", VirtualKeyCode.F8),
+            new KeyRep("f9", VirtualKeyCode.F9),
+            new KeyRep("f10", VirtualKeyCode.F10),
+            new KeyRep("f11", VirtualKeyCode.F11),
+            new KeyRep("f12", VirtualKeyCode.F12),
+            new KeyRep("tab", VirtualKeyCode.TAB),
+            new KeyRep("ctrl", VirtualKeyCode.CONTROL),
+            new KeyRep("alt", VirtualKeyCode.MENU),
+            new KeyRep("a", VirtualKeyCode.VK_A), 
+            new KeyRep("b", VirtualKeyCode.VK_B), 
+            new KeyRep("c", VirtualKeyCode.VK_C), 
+            new KeyRep("d", VirtualKeyCode.VK_D), 
+            new KeyRep("e", VirtualKeyCode.VK_E), 
+            new KeyRep("f", VirtualKeyCode.VK_F), 
+            new KeyRep("g", VirtualKeyCode.VK_G), 
+            new KeyRep("h", VirtualKeyCode.VK_H), 
+            new KeyRep("i", VirtualKeyCode.VK_I), 
+            new KeyRep("j", VirtualKeyCode.VK_J), 
+            new KeyRep("k", VirtualKeyCode.VK_K), 
+            new KeyRep("l", VirtualKeyCode.VK_L), 
+            new KeyRep("m", VirtualKeyCode.VK_M), 
+            new KeyRep("n", VirtualKeyCode.VK_N), 
+            new KeyRep("o", VirtualKeyCode.VK_O), 
+            new KeyRep("p", VirtualKeyCode.VK_P), 
+            new KeyRep("q", VirtualKeyCode.VK_Q), 
+            new KeyRep("r", VirtualKeyCode.VK_R), 
+            new KeyRep("s", VirtualKeyCode.VK_S), 
+            new KeyRep("t", VirtualKeyCode.VK_T), 
+            new KeyRep("u", VirtualKeyCode.VK_U), 
+            new KeyRep("v", VirtualKeyCode.VK_V), 
+            new KeyRep("w", VirtualKeyCode.VK_W), 
+            new KeyRep("x", VirtualKeyCode.VK_X), 
+            new KeyRep("y", VirtualKeyCode.VK_Y), 
+            new KeyRep("z", VirtualKeyCode.VK_X), 
+            new KeyRep("1", VirtualKeyCode.VK_1), 
+            new KeyRep("2", VirtualKeyCode.VK_2), 
+            new KeyRep("3", VirtualKeyCode.VK_3), 
+            new KeyRep("4", VirtualKeyCode.VK_4), 
+            new KeyRep("5", VirtualKeyCode.VK_5), 
+            new KeyRep("6", VirtualKeyCode.VK_6), 
+            new KeyRep("7", VirtualKeyCode.VK_7), 
+            new KeyRep("8", VirtualKeyCode.VK_8), 
+            new KeyRep("9", VirtualKeyCode.VK_9), 
+            new KeyRep("0", VirtualKeyCode.VK_0), 
+            new KeyRep("-", VirtualKeyCode.OEM_MINUS), 
+            new KeyRep("+", VirtualKeyCode.OEM_PLUS), 
+            new KeyRep("win", VirtualKeyCode.LWIN),
+            new KeyRep("enter", VirtualKeyCode.RETURN),
+            new KeyRep("left", VirtualKeyCode.LEFT),
+            new KeyRep("right", VirtualKeyCode.RIGHT),
+            new KeyRep("up", VirtualKeyCode.UP),
+            new KeyRep("down", VirtualKeyCode.DOWN)
         };
 
-        public VirtualKeyCode GetKeyCode(string keyString)
+        public static VirtualKeyCode GetKeyCode(string keyString)
         {
             var toreturnCode =
-                from keyStrings in keyCombos
+                from keyStrings in _keyReps
                 where keyStrings.Key == keyString
                 select keyStrings.KeyCode;
             return toreturnCode.First();
