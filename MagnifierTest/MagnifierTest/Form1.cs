cc3612a3dfbdb08f70a9a8381cf67768a394e01d
ModerateWinGuy <moderatewinguy@gmail.com>
2015-08-05 21:11:41 +1200

    Testing out the magnifier class

---
+++ MagnifierTest/MagnifierTest/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Karna.Magnification;
+
+namespace MagnifierTest
+{
+    public partial class Form1 : Form
+    {
+        private Magnifier mag;
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mag = new Magnifier(this);
+            mag.Magnification = (float)1.5;
+            mag.UpdateMaginifier();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            mag.Magnification = mag.Magnification * (float)1.1;
+        }
+    }
+}
