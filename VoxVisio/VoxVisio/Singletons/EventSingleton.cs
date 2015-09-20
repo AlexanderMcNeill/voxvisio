b33f8c503f310e715b0cff8d36299181538da62c
ModerateWinGuy <moderatewinguy@gmail.com>
2015-09-20 15:48:22 +1200

    Removing blank space

--- VoxVisio/VoxVisio/Singletons/EventSingleton.cs
+++ VoxVisio/VoxVisio/Singletons/EventSingleton.cs
@@ -31,9 +31,7 @@ namespace VoxVisio.Singletons
 
         private EyeXHost eyex;
         public event FixationEvent fixationEvent;
-
         public readonly SpeechRecognitionEngine speechRecognizer;
-
         public readonly Hook keyboardHook;
 
         protected EventSingleton()
