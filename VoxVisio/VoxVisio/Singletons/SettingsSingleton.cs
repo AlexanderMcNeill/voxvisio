24ad71e9d343c3e92b09457bcd3f96a3c7a69da1
Daniel <moderatewinguy@gmail.com>
2015-10-19 11:59:16 +1300

    Revert "Fixing merge issues"
    This reverts commit 293812a01fba768578c364a645a6edb7ae990241.

--- VoxVisio/VoxVisio/Singletons/SettingsSingleton.cs
+++ VoxVisio/VoxVisio/Singletons/SettingsSingleton.cs
@@ -20,10 +20,18 @@ namespace VoxVisio.Singletons
         private EventList<Command> commands;
         public readonly Hook keyboardHook;
         public event EventHandler CommandsChanged;
+<<<<<<< HEAD
+        public bool ZoomEnabled { get; private set; }
+        public double ZoomMagnification { get; private set; }
+        public Size ZoomFormSize { get; private set; }
+        public bool DebugEyeMouseMode { get; private set; }
+        public bool DragonEnabled { get; private set; }
+=======
         public bool ZoomEnabled { get; set; }
         public double ZoomMagnification { get; set; }
         public Size ZoomFormSize { get; set; }
         public bool DebugEyeMouseMode { get; set; }
+>>>>>>> origin/master
 
 
         protected SettingsSingleton()
@@ -36,6 +44,16 @@ namespace VoxVisio.Singletons
             
         private void loadSettings()
         {
+            string fileContents = Properties.Resources.Settings;
+            using (StringReader reader = new StringReader(fileContents))//@"Commands.json"
+            {
+                JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+                ZoomEnabled = (bool)o["zoom form"]["enabled"];
+                ZoomMagnification = (double) o["zoom form"]["magnification"];
+                ZoomFormSize = new Size((int)o["zoom form"]["width"], (int)o["zoom form"]["height"]);
+                DebugEyeMouseMode = (bool)o["eye tracking"]["debug mouse mode"];
+                DragonEnabled = (bool) o["dragon enabled"];
+            }
             ZoomEnabled = Settings.Default.ZoomEnabled;
             ZoomMagnification = Settings.Default.ZoomMagnification;
             ZoomFormSize = Settings.Default.ZoomFormSize;
@@ -44,11 +62,38 @@ namespace VoxVisio.Singletons
 
         public void saveSettings()
         {
+<<<<<<< HEAD
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+
+            using (StreamWriter sw = new StreamWriter(@"c:\jsonSettings.txt"))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                writer.Formatting = Formatting.Indented;
+                JObject obj1 = new JObject();
+                JObject obj2 = new JObject();
+                JObject obj3 = new JObject();
+
+                obj2["enabled"] = ZoomEnabled;
+                obj2["magnification"] = ZoomMagnification;
+                obj2["width"] = ZoomFormSize.Width;
+                obj2["height"] = ZoomFormSize.Height;
+                obj3["debug mouse mode"] = DebugEyeMouseMode;
+
+                obj1["zoom form"] = obj2;
+                obj1["eye tracking"] = obj3;
+                obj1["dragon enabled"] = DragonEnabled;
+                obj1.WriteTo(writer);
+
+                writer.Close();
+            }
+=======
             Settings.Default.ZoomEnabled = ZoomEnabled;
             Settings.Default.ZoomMagnification = ZoomMagnification;
             Settings.Default.ZoomFormSize = ZoomFormSize;
             Settings.Default.DebugEyeMouseMode = DebugEyeMouseMode;
             Settings.Default.Save();
+>>>>>>> origin/master
         }
 
         
